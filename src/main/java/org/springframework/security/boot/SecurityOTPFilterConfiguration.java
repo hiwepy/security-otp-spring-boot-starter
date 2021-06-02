2158f8e7dbf797aa0c849964d95c60769266a79f
wandl <hiwepy@163.com>
2021-06-02 20:39:50 +0800

    过滤器初始化优化

--- src/main/java/org/springframework/security/boot/SecurityOTPFilterConfiguration.java
+++ src/main/java/org/springframework/security/boot/SecurityOTPFilterConfiguration.java
@@ -4,6 +4,7 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.ObjectProvider;
+import org.springframework.biz.web.servlet.i18n.LocaleContextFilter;
 import org.springframework.boot.autoconfigure.AutoConfigureBefore;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.boot.autoconfigure.security.SecurityProperties;
@@ -12,14 +13,14 @@ import org.springframework.boot.context.properties.EnableConfigurationProperties
 import org.springframework.boot.context.properties.PropertyMapper;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.annotation.Order;
-import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.boot.biz.authentication.AuthenticationListener;
 import org.springframework.security.boot.biz.authentication.nested.MatchedAuthenticationEntryPoint;
 import org.springframework.security.boot.biz.authentication.nested.MatchedAuthenticationFailureHandler;
 import org.springframework.security.boot.biz.authentication.nested.MatchedAuthenticationSuccessHandler;
 import org.springframework.security.boot.biz.property.SecuritySessionMgtProperties;
 import org.springframework.security.boot.otp.authentication.OTPAuthenticationProcessingFilter;
-import org.springframework.security.boot.otp.authentication.OTPAuthenticationProvider;
+import org.springframework.security.boot.utils.WebSecurityUtils;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.web.AuthenticationEntryPoint;
@@ -44,12 +45,13 @@ public class SecurityOTPFilterConfiguration {
     	
     	private final SecurityOTPAuthcProperties authcProperties;
     	
-    	private final AuthenticationEntryPoint authenticationEntryPoint;
- 	    private final AuthenticationSuccessHandler authenticationSuccessHandler;
- 	    private final AuthenticationFailureHandler authenticationFailureHandler;
- 	    private final ObjectMapper objectMapper;
-     	private final RememberMeServices rememberMeServices;
- 		private final SessionAuthenticationStrategy sessionAuthenticationStrategy;
+    	private final LocaleContextFilter localeContextFilter;
+	    private final AuthenticationEntryPoint authenticationEntryPoint;
+	    private final AuthenticationSuccessHandler authenticationSuccessHandler;
+	    private final AuthenticationFailureHandler authenticationFailureHandler;
+	    private final ObjectMapper objectMapper;
+    	private final RememberMeServices rememberMeServices;
+		private final SessionAuthenticationStrategy sessionAuthenticationStrategy;
    		
    		public OTPWebSecurityConfigurerAdapter(
    			
@@ -57,29 +59,31 @@ public class SecurityOTPFilterConfiguration {
 				SecuritySessionMgtProperties sessionMgtProperties,
    				SecurityOTPAuthcProperties authcProperties,
 
-   				ObjectProvider<OTPAuthenticationProvider> authenticationProvider,
-   				ObjectProvider<AuthenticationManager> authenticationManagerProvider,
+   				ObjectProvider<LocaleContextFilter> localeContextProvider,
+				ObjectProvider<AuthenticationProvider> authenticationProvider,
    				ObjectProvider<AuthenticationListener> authenticationListenerProvider,
    				ObjectProvider<MatchedAuthenticationEntryPoint> authenticationEntryPointProvider,
    				ObjectProvider<MatchedAuthenticationSuccessHandler> authenticationSuccessHandlerProvider,
    				ObjectProvider<MatchedAuthenticationFailureHandler> authenticationFailureHandlerProvider,
    				ObjectProvider<ObjectMapper> objectMapperProvider,
-   				ObjectProvider<RememberMeServices> rememberMeServicesProvider
+   				ObjectProvider<RememberMeServices> rememberMeServicesProvider,
+   				ObjectProvider<SessionAuthenticationStrategy> sessionAuthenticationStrategyProvider
 
 			) {
    			
-   			super(bizProperties, authcProperties, sessionMgtProperties, authenticationProvider.stream().collect(Collectors.toList()),
-					authenticationManagerProvider.getIfAvailable());
+   			super(bizProperties, sessionMgtProperties, authenticationProvider.stream().collect(Collectors.toList()));
    			
    			this.authcProperties = authcProperties;
    			
+   			this.localeContextFilter = localeContextProvider.getIfAvailable();
    			List<AuthenticationListener> authenticationListeners = authenticationListenerProvider.stream().collect(Collectors.toList());
-   			this.authenticationEntryPoint = super.authenticationEntryPoint(authenticationEntryPointProvider.stream().collect(Collectors.toList()));
-   			this.authenticationSuccessHandler = super.authenticationSuccessHandler(authenticationListeners, authenticationSuccessHandlerProvider.stream().collect(Collectors.toList()));
-   			this.authenticationFailureHandler = super.authenticationFailureHandler(authenticationListeners, authenticationFailureHandlerProvider.stream().collect(Collectors.toList()));
+   			this.authenticationEntryPoint = WebSecurityUtils.authenticationEntryPoint(authcProperties, sessionMgtProperties, authenticationEntryPointProvider.stream().collect(Collectors.toList()));
+   			this.authenticationSuccessHandler = WebSecurityUtils.authenticationSuccessHandler(authcProperties, sessionMgtProperties, authenticationListeners, authenticationSuccessHandlerProvider.stream().collect(Collectors.toList()));
+   			this.authenticationFailureHandler = WebSecurityUtils.authenticationFailureHandler(authcProperties, sessionMgtProperties, authenticationListeners, authenticationFailureHandlerProvider.stream().collect(Collectors.toList()));
    			this.objectMapper = objectMapperProvider.getIfAvailable();
    			this.rememberMeServices = rememberMeServicesProvider.getIfAvailable();
-   			this.sessionAuthenticationStrategy = super.sessionAuthenticationStrategy();
+   			this.sessionAuthenticationStrategy = sessionAuthenticationStrategyProvider.getIfAvailable();
+   			
    		}
    		   		
    	    public OTPAuthenticationProcessingFilter authenticationProcessingFilter() throws Exception {
@@ -111,13 +115,13 @@ public class SecurityOTPFilterConfiguration {
    	    @Override
 		public void configure(HttpSecurity http) throws Exception {
    	    	
-   	    	http.exceptionHandling()
+   	    	http.antMatcher(authcProperties.getPathPattern())
+				.exceptionHandling()
 	        	.authenticationEntryPoint(authenticationEntryPoint)
 	        	.and()
 	        	.httpBasic()
-	        	.authenticationEntryPoint(authenticationEntryPoint)
-	        	.and()
-	        	.antMatcher(authcProperties.getPathPattern())
+	        	.disable()
+	        	.addFilterBefore(localeContextFilter, UsernamePasswordAuthenticationFilter.class)
    	    		.addFilterBefore(authenticationProcessingFilter(), UsernamePasswordAuthenticationFilter.class);
 
    	    	super.configure(http, authcProperties.getCors());
